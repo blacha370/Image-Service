1e8a31e086a407c1d835b2fb1f80abf0ea7eef4e
blacha370 <lukasz.blacha370@gmail.com>
2021-06-18 16:46:50 +0200

    Create basic models

--- image_app/models.py
+++ image_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ThumbnailSize(models.Model):
+    height = models.IntegerField(unique=True, validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return str(self.height)
+
+
+class AccountTierClass(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    original_image = models.BooleanField(default=False)
+    expires_link = models.BooleanField(default=False)
+    thumbnail_sizes = models.ManyToManyField(ThumbnailSize)
+
+    def __str__(self):
+        return self.name
+
+
+class AccountTier(models.Model):
+    tier = models.ForeignKey(AccountTierClass, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.tier) + ': ' + self.user.username
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    url = models.URLField(max_length=200, unique=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Thumbnail(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    url = models.URLField(max_length=210, unique=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    thumbnail_size = models.ForeignKey(ThumbnailSize, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
